e0cef0db6e8014e1aed234c3a01c1340260bb0be
Sohil <savla.sohil@gmail.com>
2019-02-23 18:43:59 -0500

    02 ready to be deployed

--- 02_pascal.py
+++ 02_pascal.py
@@ -61,14 +61,17 @@ class SimpleCNN(keras.Model):
 
 def test(model, dataset):
     test_loss = tfe.metrics.Mean()
-    test_accuracy = tfe.metrics.Accuracy()
+    test_accuracy = tfe.metrics.BinaryAccuracy(threshold=0.7)
     for batch, (images, labels, weights) in enumerate(dataset):
         logits = model(images)
         loss_value = tf.losses.sigmoid_cross_entropy(labels, logits)
-        prediction = logits
-        test_loss(loss_value)
-        mAP = util.eval_dataset_map(model, dataset)
-    return test_loss.result(), mAP
+        prediction = tf.nn.sigmoid(logits)
+        test_loss(loss_value,weights=weights)
+        test_accuracy(labels=labels, predictions=prediction, weights=weights)
+
+    AP, mAP = util.eval_dataset_map(model, dataset)
+
+    return test_loss.result(), test_accuracy.result(), mAP
 
 def logging_variable(name, value):
     with tf.contrib.summary.always_record_summaries():
@@ -101,9 +104,7 @@ def main():
 
     train_images, train_labels, train_weights = util.load_pascal(args.data_dir,
                                                                  class_names=CLASS_NAMES,
-                                                                 split='trainval
-                                                                 
-                                                                 ')
+                                                                 split='trainval')
     test_images, test_labels, test_weights = util.load_pascal(args.data_dir,
                                                               class_names=CLASS_NAMES,
                                                               split='test')
@@ -135,22 +136,24 @@ def main():
     ## TODO write the training and testing code for multi-label classification
     global_step = tf.train.get_or_create_global_step()
     optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
-    train_log = {'iter': [], 'loss': [], 'map': []}
-    test_log = {'iter': [], 'loss': [], 'map': []}
+    train_log = {'iter': [], 'loss': [], 'accuracy': []}
+    test_log = {'iter': [], 'loss': [], 'map': [], 'accuracy': []}
     for ep in range(args.epochs):
         epoch_loss_avg = tfe.metrics.Mean()
-        # epoch_accuracy = tfe.metrics.Accuracy()
+        epoch_accuracy = tfe.metrics.BinaryAccuracy(threshold=0.7)
         for batch, (images, labels, weights) in enumerate(train_dataset):
             loss_value, grads = util.cal_grad(model,
                                               loss_func=tf.losses.sigmoid_cross_entropy,
                                               inputs=images,
-                                              targets=labels)
+                                              targets=labels,
+                                              weights=weights)
             optimizer.apply_gradients(zip(grads,
                                           model.trainable_variables),
                                       global_step)
-            epoch_loss_avg(loss_value)
-            # print("Batch: ",batch)
-            # epoch_accuracy(predictions=tf.nn.sigmoid(model(images)),labels=labels)
+            epoch_loss_avg(loss_value, weights=weights)
+            pred = tf.nn.sigmoid(model(images))
+            epoch_accuracy(predictions=pred,labels=labels,weights=weights)
+            print("Batch: ",batch)
             if global_step.numpy() % args.log_interval == 0:
                 print('Epoch: {0:d}/{1:d} Iteration:{2:d}  Training Loss:{3:.4f}  '.format(ep,
                                                          args.epochs,
@@ -158,35 +161,25 @@ def main():
                                                          epoch_loss_avg.result()))
                 train_log['iter'].append(global_step.numpy())
                 train_log['loss'].append(epoch_loss_avg.result())
+                train_log['accuracy'].append(epoch_accuracy.result())
+                # Logging for TensorFlow
                 logging_variable('train_loss',epoch_loss_avg.result())
-                # train_log['accuracy'].append(epoch_accuracy.result())
+                logging_variable('train_accuracy',epoch_accuracy.result())
             if global_step.numpy() % args.eval_interval == 0:
-                AP, mAP = util.eval_dataset_map(model, test_dataset)
-                # test_loss, test_map = test(model, test_dataset)
+                # AP, mAP = util.eval_dataset_map(model, test_dataset)
+                test_loss, test_accuracy, mAP = test(model, test_dataset)
                 test_log['iter'].append(global_step.numpy())
-                # test_log['loss'].append(test_loss)
+                test_log['loss'].append(test_loss)
+                test_log['accuracy'].append(test_accuracy)
                 test_log['map'].append(mAP)
+                # Logging for TensorFlow
                 logging_variable('test_mAP',mAP)
+                logging_variable('test_loss',test_loss)
+                logging_variable('test_accuracy',test_accuracy)
 
     model.summary()
     end_time = time.time()
     print('Elapsed time: {0:.3f}s'.format(end_time - start_time))
-    predict(model, test_images[:5], class_names)
-
-
-    # fig = plt.figure()
-    # plt.plot(train_log['iter'], train_log['loss'], 'r', label='Training')
-    # plt.plot(test_log['iter'], test_log['loss'], 'b', label='Testing')
-    # plt.title('Loss')
-    # plt.legend()
-    # plt.savefig("results/02_loss.png")
-    # fig = plt.figure()
-    # plt.plot(train_log['iter'], train_log['accuracy'], 'r', label='Training')
-    # plt.plot(test_log['iter'], test_log['accuracy'], 'b', label='Testing')
-    # plt.title('Accuracy')
-    # plt.legend()
-    # plt.savefig("results/02_accuracy.png")
-    # plt.show()
 
     np.save("02_training.npy", train_log)
     np.save("02_test.npy", test_log)
