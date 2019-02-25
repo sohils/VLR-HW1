b9b99b340b028d8084f0ea504ae6b261d9cd7b96
Sohil <savla.sohil@gmail.com>
2019-02-25 18:36:39 -0500

    Model is loading weights

--- 05_pascal_vgg_finetune.py
+++ 05_pascal_vgg_finetune.py
@@ -12,6 +12,7 @@ from tensorflow.contrib import eager as tfe
 from tensorflow.keras import layers
 
 import time
+import h5py
 
 import util
 import matplotlib.pyplot as plt
@@ -22,37 +23,99 @@ CLASS_NAMES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
 
 IMAGENET_MEAN = np.array([123.68, 116.81, 103.94],dtype="float32")
 
-
-class SimpleCNN(keras.Model):
+class VGG16(keras.Model):
     def __init__(self, num_classes=10):
-        super(SimpleCNN, self).__init__(name='SimpleCNN')
+        super(VGG16, self).__init__(name='VGG16')
         self.num_classes = num_classes
-        self.conv1 = layers.Conv2D(filters=32,
-                                   kernel_size=[5, 5],
+        self.conv1_1 = layers.Conv2D(filters=64,
+                                   kernel_size=[3, 3],
                                    padding="same",
                                    activation='relu')
-        self.pool1 = layers.MaxPool2D(pool_size=(2, 2))
-        self.conv2 = layers.Conv2D(filters=64,
-                                   kernel_size=[5, 5],
+        self.conv1_2 = layers.Conv2D(filters=64,
+                                   kernel_size=[3, 3],
                                    padding="same",
                                    activation='relu')
-        self.pool2 = layers.MaxPool2D(pool_size=(2, 2))
-
+        self.pool1 = layers.MaxPool2D(pool_size=(2, 2), strides=(2,2))
+        self.conv2_1 = layers.Conv2D(filters=128,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv2_2 = layers.Conv2D(filters=128,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.pool2 = layers.MaxPool2D(pool_size=(2, 2), strides=(2,2))
+        self.conv3_1 = layers.Conv2D(filters=256,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv3_2 = layers.Conv2D(filters=256,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv3_3 = layers.Conv2D(filters=256,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.pool3 = layers.MaxPool2D(pool_size=(2, 2), strides=(2,2))
+        self.conv4_1 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv4_2 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv4_3 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.pool4 = layers.MaxPool2D(pool_size=(2, 2), strides=(2,2))
+        self.conv5_1 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv5_2 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.conv5_3 = layers.Conv2D(filters=512,
+                                   kernel_size=[3, 3],
+                                   padding="same",
+                                   activation='relu')
+        self.pool5 = layers.MaxPool2D(pool_size=(2, 2), strides=(2,2))
         self.flat = layers.Flatten()
-
-        self.dense1 = layers.Dense(1024, activation='relu')
-        self.dropout = layers.Dropout(rate=0.4)
-        self.dense2 = layers.Dense(num_classes)
+        self.dense1 = layers.Dense(4096, activation='relu')
+        self.dropout1 = layers.Dropout(rate=0.5)
+        self.dense2 = layers.Dense(4096, activation='relu')
+        self.dropout2 = layers.Dropout(rate=0.5)
+        self.dense3 = layers.Dense(num_classes)
 
     def call(self, inputs, training=False):
-        x = self.conv1(inputs)
+        x = self.conv1_1(inputs)
+        x = self.conv1_2(x)
         x = self.pool1(x)
-        x = self.conv2(x)
+        x = self.conv2_1(x)
+        x = self.conv2_2(x)
         x = self.pool2(x)
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        x = self.conv3_3(x)
+        x = self.pool3(x)
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+        x = self.conv4_3(x)
+        x = self.pool4(x)
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
+        x = self.conv5_3(x)
+        x = self.pool5(x)
         flat_x = self.flat(x)
         out = self.dense1(flat_x)
-        out = self.dropout(out, training=training)
+        out = self.dropout1(out, training=training)
         out = self.dense2(out)
+        out = self.dropout2(out, training=training)
+        out = self.dense3(out)
         return out
 
     def compute_output_shape(self, input_shape):
@@ -61,6 +124,9 @@ class SimpleCNN(keras.Model):
         return tf.TensorShape(shape)
 
 def test(model, dataset):
+    preds = []
+    gts = []
+    valids = []
     test_loss = tfe.metrics.Mean()
     test_accuracy = tfe.metrics.BinaryAccuracy(threshold=0.7)
     for batch, (images, labels, weights) in enumerate(dataset):
@@ -68,15 +134,30 @@ def test(model, dataset):
         loss_value = tf.losses.sigmoid_cross_entropy(labels, logits)
         prediction = tf.nn.sigmoid(logits)
         test_loss(loss_value,weights=weights)
-        test_accuracy(labels=labels, predictions=prediction, weights=weights)
-
-    AP, mAP = util.eval_dataset_map(model, dataset)
-
+        test_accuracy(labels=tf.cast(labels,tf.bool), predictions=prediction, weights=weights)
+        preds.append(prediction.numpy())
+        gts.append(labels.numpy())
+        valids.append(weights.numpy())
+    # Stack all the predicitons, labels and weights
+    preds = np.vstack(preds)
+    gts = np.vstack(gts)
+    valids = np.vstack(valids)
+    # Calculate mAP
+    AP, mAP = util.eval_dataset_map_helper(gts, preds, valids)
     return test_loss.result(), test_accuracy.result(), mAP
 
 def logging_variable(name, value):
     with tf.contrib.summary.always_record_summaries():
         tf.contrib.summary.scalar(name, value)
+
+def logging_histogram(name, tensor):
+    with tf.contrib.summary.always_record_summaries():
+        for index, g in enumerate(tensor):
+            tf.contrib.summary.histogram(str(index)+"-grad" , g) 
+
+def logging_image(name, tensor):
+    with tf.contrib.summary.always_record_summaries():
+        tf.contrib.summary.image(name=name, tensor=tensor, max_images=1)
 
 
 def main():
@@ -102,6 +183,31 @@ def main():
     args = parser.parse_args()
     util.set_random_seed(args.seed)
     sess = util.set_session()
+    filename = './data/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
+    f = h5py.File(filename, 'r')
+
+    # demo_model = keras.models.Model()
+    # demo_model.build(input_shape=(1,224,224,3))
+    # demo_model.load_weights(filepath='./data/vgg16_weights_tf_dim_ordering_tf_kernels.h5', by_name=False)
+
+    model = VGG16(num_classes=len(CLASS_NAMES))
+    model.build(input_shape=(1,224,224,3))
+    my_model_index = 0
+    for index, l in enumerate(list(f.keys())):
+        weights = []
+        for _, w in enumerate(list(f[l])):
+            weights.append(f[l][w])
+        while(model.layers[my_model_index].name == 'flatten' or model.layers[my_model_index].name == 'dropout'):
+            my_model_index+=1
+        model.layers[my_model_index].set_weights(weights)
+        my_model_index+=1
+        if(l=='fc2'):
+            break
+
+    for index in len(demo_model.layers):
+        a = demo_model.get_layer(index).get_weights()
+
+    model.load_weights(filepath='./data/vgg16_weights_tf_dim_ordering_tf_kernels.h5', by_name=False)
 
     train_images, train_labels, train_weights = util.load_pascal(args.data_dir,
                                                                  class_names=CLASS_NAMES,
@@ -113,15 +219,18 @@ def main():
     ## TODO modify the following code to apply data augmentation here
     train_images = train_images - IMAGENET_MEAN
     test_images = test_images - IMAGENET_MEAN
-
+    print("Converting data to tensors")
     train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels, train_weights))
     train_dataset = util.data_augmentation(train_dataset,args.seed)
     train_dataset = train_dataset.shuffle(10000).batch(args.batch_size)
     test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels, test_weights))
-    test_dataset = util.data_augmentation(test_dataset,args.seed)
+    test_dataset = test_dataset.map(lambda x,y,z: util.center_crop(x,y,z))
+    # test_dataset = util.data_augmentation(test_dataset,args.seed)
     test_dataset = test_dataset.shuffle(10000).batch(args.batch_size)
-
-    model = SimpleCNN(num_classes=len(CLASS_NAMES))
+    
+    del(train_images)
+    del(test_images)
+    print("Deleting original data")
 
     # Logging block
     logdir = os.path.join(args.log_dir,
